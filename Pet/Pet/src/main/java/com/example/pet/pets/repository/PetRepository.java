ff5784a5b00574b7c14e8cfd05f34f4ff420b4d3
Felipe Morais <moraisfelipe97@gmail.com>
2021-08-03 20:31:53 -0300

    criado /all/owner-id

--- Pet/Pet/src/main/java/com/example/pet/pets/repository/PetRepository.java
+++ Pet/Pet/src/main/java/com/example/pet/pets/repository/PetRepository.java
@@ -1,9 +1,13 @@
 package com.example.pet.pets.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.pet.pets.model.Pet;
 
 public interface PetRepository extends JpaRepository<Pet, Long>{
 
+	List<Pet> findByOwnerId(Long ownerId);
+	
 }
